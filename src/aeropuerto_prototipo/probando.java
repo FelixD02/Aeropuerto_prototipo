2475889d0c2194846d5ecc2e24bae9e673d416dc
sebaslagu <sebaslagu12@gmail.com>
2023-01-13 21:05:11 -0500

    prueba de git pues

---
+++ src/aeropuerto_prototipo/probando.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aeropuerto_prototipo;
+
+/**
+ *
+ * @author usuario
+ */
+public class probando {
+    private String si = "probandosi";
+}
